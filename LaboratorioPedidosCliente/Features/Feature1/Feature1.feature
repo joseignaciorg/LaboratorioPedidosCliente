1fe5302a11a4561312ccb4f1d40fb380dada7d0d
Nacho <joseignacio.rodriguez@tajamar365.com>
2016-04-11 13:26:29 +0000

    Crear un control para ver los pedidos que ha realizado un determinado cliente, para ello usaremos una acción personalizada.

---
+++ LaboratorioPedidosCliente/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="328914f5-d764-4c27-9a39-0d4861c428f2" description="SharePoint Add-in Feature" featureId="328914f5-d764-4c27-9a39-0d4861c428f2" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="LaboratorioPedidosCliente Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e0fdf512-8669-42ed-8030-c0308baa0b08" />
+    <projectItemReference itemId="d842f05b-d930-43ce-830a-e63163d63d13" />
+    <projectItemReference itemId="28c8ec62-bd9d-43a6-ad0c-ed7625ba2f10" />
+    <projectItemReference itemId="6fd62a9c-a87c-425e-80d1-1c232b520aaf" />
+  </projectItems>
+</feature>
---
+++ LaboratorioPedidosCliente/pkg/Debug/LaboratorioPedidosCliente/LaboratorioPedidosCliente_Feature1/Scripts/App.js
@@ -0,0 +1,129 @@
+﻿
+//funcion que nos devuelve los parametros de la url
+function getQueryStringParams(sParam) {
+
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+var currentContext;
+var hostWebContext;
+var web;
+var list;
+
+//Funcion que nos duvelve los pedidos del cliente actual y sus correspondientes funciones de callback
+function getList(id) {
+    clearInfo();
+    var lista = hostWebContext.get_web().get_lists().getByTitle("PedidosClientes");
+
+    var query = new SP.CamlQuery();
+    query.set_viewXml("<View Scope='RecursiveAll'><Query><Where><Contains><FieldRef Name='Cliente_x003a_ID' /><Value Type='Text'>" + id + "</Value></Contains></Where></Query></View>");
+    list = lista.getItems(query);
+    currentContext.load(list);
+    currentContext.executeQueryAsync(Function.createDelegate(this, onSuccess), Function.createDelegate(this, onFail));
+}
+
+//funicon de callback de satisfactorio
+function onSuccess() {
+    if (list.get_count() != 0) {
+        var listEnum = list.getEnumerator();
+        var n = 1;
+        var total = 0;
+        var tabla = $("#TablaPedidos tbody");
+        while (listEnum.moveNext()) {
+            var html = "<tr>";
+            var actual = listEnum.get_current();
+            var d = actual.get_item("Fecha");
+            var day = d.getDate();
+            var month = d.getMonth() + 1;
+            var year = d.getFullYear();
+            html += "<td>" + actual.get_item("NumeroPedido") + "</td>";
+            html += "<td>" + day + "-" + month + "-" + year + "</td>";
+            html += "<td>" + actual.get_item("Total") + "€ </td>";
+            n++;
+            tabla.append(html);
+        }
+
+        var nombre = actual.get_item("Cliente_x003a_Nombre_x0020_compl").get_lookupValue();
+        $("#Nombre").text(nombre);
+        $("#NumeroPedidos").text("Pedidos: " + (n - 1));
+        $("#MostrarPedidos").css("display", "block");
+        $("#SinPedidos").css("display", "none");
+    }
+    else {
+        $("#MostrarPedidos").css("display", "none");
+        $("#SinPedidos").css("display", "block");
+    }
+}
+
+//funcion de callback de error
+function onFail() {
+    alert("Error");
+}
+
+//funcion para limpiar informacion
+function clearInfo() {
+    var t = $("#TablaPedidos>tbody>tr");
+    t.remove();
+    $("#NumeroPedidos").text("");
+    $("#Total").text("");
+}
+
+function init() {
+
+    var hostUrl = decodeURIComponent(getQueryStringParams("SPHostUrl"));
+
+    currentContext = new SP.ClientContext.get_current();
+
+    hostWebContext = new SP.AppContextSite(currentContext, hostUrl);
+
+    web = hostWebContext.get_web();
+
+    var id = getQueryStringParams("SPListItemId");
+
+    getList(id);
+
+}
+
+$(document).ready(function () {
+    ExecuteOrDelayUntilScriptLoaded(init, "sp.js");
+});
+
+
+//'use strict';
+
+//ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+//function initializePage()
+//{
+//    var context = SP.ClientContext.get_current();
+//    var user = context.get_web().get_currentUser();
+
+//    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+//    $(document).ready(function () {
+//        getUserName();
+//    });
+
+//    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+//    function getUserName() {
+//        context.load(user);
+//        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//    }
+
+//    // This function is executed if the above call is successful
+//    // It replaces the contents of the 'message' element with the user name
+//    function onGetUserNameSuccess() {
+//        $('#message').text('Hello ' + user.get_title());
+//    }
+
+//    // This function is executed if the above call fails
+//    function onGetUserNameFail(sender, args) {
+//        alert('Failed to get user name. Error:' + args.get_message());
+//    }
+//}
